9a5f3ee329ef5f5c44f47d5a0f2b2ce2e31a0fd3
gokulhansv <gokul.h@enke.in>
2024-06-01 09:50:21 +0530

    new

--- server/app.js
+++ server/app.js
@@ -21,11 +21,18 @@ app.use(express.urlencoded({ extended: true }));
 //     } else {
 //       callback(new Error("Not allowed by CORS"));
 //     }
-//   }, 
+//   },
 //   credentials: true,
 // };
 // app.use(cors(corsOptions));
-app.use(cors());
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(logger("dev"));
 
