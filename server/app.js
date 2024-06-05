b63ce8f993ca3da6089b616f6f66cdd6b46cf438
gokulhansv <gokul.h@enke.in>
2024-06-05 23:42:58 +0530

    ready to host

--- server/app.js
+++ server/app.js
@@ -29,9 +29,9 @@ app.use(
 // };
 
 const corsOptions = {
-  // origin: "https://hela-ecommerce.vercel.app",
+  origin: "https://hela-ecommerce.vercel.app",
   // origin: process.env.CLIENT_URL,
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
