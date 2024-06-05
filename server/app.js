b9d9a98e3eb56a9817d2f31b675aa973bc4a4859
gokulhansv <gokul.h@enke.in>
2024-06-05 18:58:25 +0530

    middleware removed

--- server/app.js
+++ server/app.js
@@ -52,8 +52,10 @@ const { requireAuth, requireAdminAuth } = require("./middleware/requireAuth");
 // Mounting the routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user",  userRoutes);
-app.use("/api/admin", requireAdminAuth, adminRoutes);
-app.use("/api/super-admin", requireAdminAuth, superAdminRoutes);
+// app.use("/api/admin", requireAdminAuth, adminRoutes);
+// app.use("/api/super-admin", requireAdminAuth, superAdminRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/super-admin", superAdminRoutes);
 app.use("/api/public", publicRoutes);
 
 // Public Api for accessing images
