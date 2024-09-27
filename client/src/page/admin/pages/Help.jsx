5ac22b6cd40fce93b3e921c772c4657ae0e23b72
Gokul Hans <gokulhansv@gmail.com>
2024-09-27 22:58:49 +0530

    help contact updated

--- client/src/page/admin/pages/Help.jsx
+++ client/src/page/admin/pages/Help.jsx
@@ -4,7 +4,7 @@ const Help = () => {
   return (
     <div className="p-5">
       <h1 className="font-bold text-2xl">Help</h1>
-      <p>Please mail at: faiz@gmail.com</p>
+      <p>Please whatsapp us at +91 8921992747</p>
     </div>
   );
 };
