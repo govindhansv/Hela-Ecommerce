6c07165ff1afc463f3fb7817ba687e47e2697abb
govindhansv <govindhansv333@gmail.com>
2025-11-06 16:12:25 +0530

    category page design change

--- client/src/components/Cards/ProductCard3.jsx
+++ client/src/components/Cards/ProductCard3.jsx
@@ -5,24 +5,34 @@ import { Link } from "react-router-dom";
 
 const ProductCard3 = ({ item }) => {
   return (
-    <div className="relative w-full">
-      <div className="pt-[100%]"></div>
-      <div
-        className="absolute inset-0 bg-cover bg-center flex flex-col justify-between p-4"
-        style={{ backgroundImage: `url('${URL}/img/${item.imgURL}')` }}
-      >
-        <div className="text-[20px] font-bold sm:text-[24px] md:text-[26px] text-white text-left">
-          {item.name}
-        </div>
-        <div>
           <Link to={`/collections?category=${item._id}`}>
-            <Button className="bg-transparent border-[1px] mt-3 sm:w-[176px] sm:h-[50px] rounded-[5px] font-Inter text-[16px] sm:text-[18px] hover:opacity-100 hover:bg-white hover:text-[#2C2C2C] text-white">
+
+    <div className="w-full">
+      {/* Image Container */}
+      <div className="relative w-full">
+        <div className="pt-[100%]"></div>
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: `url('${URL}/img/${item.imgURL}')` }}
+        ></div>
+      </div>
+
+      {/* Content Below Image */}
+      <div className="p-4">
+        {/* <div className="text-[20px] font-bold sm:text-[24px] md:text-[26px] text-[#701627] text-center mb-3">
+          {item.name}
+        </div> */}
+        <div className="flex justify-center">
+            {/* <Button className="bg-[#701627] border-[1px] border-gray-800 sm:w-[176px] sm:h-[50px] rounded-[5px] font-Inter text-[16px] sm:text-[18px] hover:bg-transparent hover:text-gray-800 text-white transition-colors">
               View
-            </Button>
-          </Link>
+            </Button> */}
+            <div className="text-[20px] font-bold sm:text-[24px] md:text-[26px] text-[#701627] text-center mb-3">
+              {item.name}
+            </div>
         </div>
       </div>
     </div>
+          </Link>
   );
 };
 
